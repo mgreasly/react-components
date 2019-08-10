25dbfafb9c352a7bc177b11d8ab616449110bad0
Marcus Greasly <marcus.greasly@avanade.com>
2019-08-10 17:08:02 +1000

    Update to components

--- components/ApiWrapper.js
+++ components/ApiWrapper.js
@@ -2,13 +2,8 @@ import React, { cloneElement, useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default ({url, children}) => {
-
     const [ response, setResponse ] = useState(false);
-
-    useEffect( () => {
-        axios.get(url).then(r => setResponse(r.data));
-    }, [url]);
-
+    useEffect(() => { axios.get(url).then(r => setResponse(r.data)); }, [url]);
     return (
         <div>
             {[].concat(children).map((child, index) => cloneElement(child, { "data": response, "key": index}))}
